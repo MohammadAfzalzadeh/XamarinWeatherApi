a120b2699035467490b29e2392314f6a0be62876
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-05 22:26:32 +0330

    Add Popup page for show err

---
+++ Weather/Weather/Views/Popup/ErrPopupPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using FreshMvvm;
+using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+
+
+
+
+namespace Weather.Views.Popup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ErrPopupPage : PopupPage
+    {
+        public ErrPopupPage(String err)
+        {
+            InitializeComponent();
+            errlbl.Text = err;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopPopupAsync();
+        }
+    }
+
+}
