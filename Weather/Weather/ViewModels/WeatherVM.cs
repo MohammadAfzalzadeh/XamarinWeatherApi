0f7c6cbc701baf4cb1137353b2e84434c8fb42b4
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-05 22:25:48 +0330

    add Weather VM

---
+++ Weather/Weather/ViewModels/WeatherVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using Weather.Models;
+using Weather.Models.API;
+
+
+namespace Weather.ViewModels
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public WeatherInfo WeatherInfo
+        {
+            get
+            {
+                return _weatherInfo;
+            }
+            private set
+            {
+                _weatherInfo = value;
+                onPropertyChange(nameof(WeatherInfo));
+            }
+        }
+        private WeatherInfo _weatherInfo { get; set; }
+
+        public WeatherVM()
+        {
+        }
+
+        void onPropertyChange(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+        public async System.Threading.Tasks.Task setWeatherInfoAsync(string city)
+        {
+            WeatherInfo = await new WeatherApi().getWeatherOfCity(city);
+        }
+
+    }
+}
