42758d57a7f85fbd9ea2829742866cb4c9e2ef10
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-06 14:00:31 +0330

    add loading Popup

---
+++ Weather/Weather/Views/Popup/LoadingPopupPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Weather.Views.Popup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadingPopupPage : PopupPage
+    {
+        public LoadingPopupPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            base.OnBackButtonPressed();
+            return true;
+        }
+    }
+}
