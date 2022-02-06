41e65b976b9eeffd0c149a4d7f53a36e0f5b61fb
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-06 14:14:30 +0330

    Add VM for Tranlate

---
+++ Weather/Weather/ViewModels/TranslationVM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Models;
+using Weather.Models.API;
+
+namespace Weather.ViewModels
+{
+    public class TranslationVM
+    {
+        public TranslateInfo translateInfo { get; set; }
+
+        public async Task<string> translateToEn(string phrase)
+        {
+            return await (new TranslateApi().traslateAllToEnglish(phrase));
+        }
+    }
+}
