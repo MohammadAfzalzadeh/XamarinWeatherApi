1c806b99c8689bb7a5e6f0403db48ef3f170215a
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-06 14:19:32 +0330

    add Popup loading and once click on btn

--- Weather/Weather/Views/WeatherPage.xaml.cs
+++ Weather/Weather/Views/WeatherPage.xaml.cs
@@ -18,83 +18,96 @@ namespace Weather.Views
         public WeatherPage()
         {
             InitializeComponent();
+            mainGrid.IsVisible = false;
             weather = new WeatherVM();
             BindingContext = weather;
         }
-
+        
         private async void Button_Clicked(object sender, EventArgs e)
         {
-
-            if (cityTxt.Text != null && cityTxt.Text != "")
-                try
-                {
-                await  weather.setWeatherInfoAsync(cityTxt.Text);
-
-                    switch (weather.WeatherInfo.weather[0].icon)
+            await weather.run(async () =>
+            {
+                mainGrid.IsVisible = false;
+                await Task.Delay(500);
+                if (cityTxt.Text != null && cityTxt.Text != "")
+                    try
                     {
-                        case "01d":
-                            WeatherImg.Animation = "sun.json";
-                            break;
-                        case "01n":
-                            WeatherImg.Animation = "moon.json";
-                            break;
-                        case "02n":
-                            WeatherImg.Animation = "moonCld.json";
-                            break;
-                        case "02d":
-                            WeatherImg.Animation = "sunCld.json";
-                            break;
-                        case "03n":
-                        case "03d":
-                        case "04n":
-                        case "04d":
-                            WeatherImg.Animation = "cld.json";
-                            break;
-                        case "09n":
-                        case "09d":
-                            WeatherImg.Animation = "rain.json";
-                            break;
-                        case "10n":
-                            WeatherImg.Animation = "moonRain.json";
-                            break;
-                        case "10d":
-                            WeatherImg.Animation = "sunRain.json";
-                            break;
-                        case "11n":
-                        case "11d":
-                            WeatherImg.Animation = "rad.json";
-                            break;
-                        case "13n":
-                        case "13d":
-                            WeatherImg.Animation = "sonw.json";
-                            break;
-                        case "50n":
-                        case "50d":
-                            WeatherImg.Animation = "mist.json";
-                            break;
+                        await Navigation.PushPopupAsync(new Popup.LoadingPopupPage());
+                        string enCity = await new TranslationVM().translateToEn(cityTxt.Text);
+                        await weather.setWeatherInfoAsync(enCity);
 
-                        default:
-                            WeatherImg.Animation =
-                            "https://openweathermap.org/img/wn/" 
-                            + weather.WeatherInfo.weather[0].icon + "@2x.png";
-                            break;
+                        switch (weather.WeatherInfo.weather[0].icon)
+                        {
+                            case "01d":
+                                WeatherImg.Animation = "sun.json";
+                                break;
+                            case "01n":
+                                WeatherImg.Animation = "moon.json";
+                                break;
+                            case "02n":
+                                WeatherImg.Animation = "moonCld.json";
+                                break;
+                            case "02d":
+                                WeatherImg.Animation = "sunCld.json";
+                                break;
+                            case "03n":
+                            case "03d":
+                            case "04n":
+                            case "04d":
+                                WeatherImg.Animation = "cld.json";
+                                break;
+                            case "09n":
+                            case "09d":
+                                WeatherImg.Animation = "rain.json";
+                                break;
+                            case "10n":
+                                WeatherImg.Animation = "moonRain.json";
+                                break;
+                            case "10d":
+                                WeatherImg.Animation = "sunRain.json";
+                                break;
+                            case "11n":
+                            case "11d":
+                                WeatherImg.Animation = "rad.json";
+                                break;
+                            case "13n":
+                            case "13d":
+                                WeatherImg.Animation = "sonw.json";
+                                break;
+                            case "50n":
+                            case "50d":
+                                WeatherImg.Animation = "mist.json";
+                                break;
+
+                            default:
+                                WeatherImg.Animation =
+                                    weather.WeatherInfo.weather[0].fullAddresIcon;
+                                break;
+                        }
+
+                        citylbl.Text = weather.WeatherInfo.Sys.country + " , " + weather.WeatherInfo.name;
+                        mainGrid.IsVisible = true;
+                        await Navigation.PopPopupAsync();
                     }
+                    catch (System.Net.Http.HttpRequestException httpEx)
+                    {
+                        await Navigation.PopPopupAsync();
+                        if (httpEx.ToString().Contains("404"))
+                            await Navigation.PushPopupAsync(new Popup.ErrPopupPage("This City isn't Exist"));
 
-                }
-                catch (System.Net.Http.HttpRequestException ex)
-                {
-                    if (ex.ToString().Contains("404"))
-                        await Navigation.PushPopupAsync(new Popup.ErrPopupPage("This City isn't Exist"));
-                        
-                    else
-                        await Navigation.PushPopupAsync(new Popup.ErrPopupPage("Can not connect to internet"));
-                        
-                       
+                        else
+                            await Navigation.PushPopupAsync(new Popup.ErrPopupPage("Can not connect to internet"));
+                    }
+                    catch (Exception ex)
+                    {
+                        await Navigation.PopPopupAsync();
+                        await Navigation.PushPopupAsync(new Popup.ErrPopupPage(ex.ToString()));
+                    }
+                else
+                    await Navigation.PushPopupAsync(new Popup.ErrPopupPage("Please Enter a city name"));
 
-                }
-            else
-                 await Navigation.PushPopupAsync(new Popup.ErrPopupPage("Please Enter a city name"));
-
+            });
         }
+
     }
 }
