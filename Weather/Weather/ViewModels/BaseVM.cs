844f4366e936b1ba490eaf348f01b9e68455fef6
mohammad_afzalzadeh <mohammad.afzalzadeh.3@gmail.com>
2022-02-06 14:13:48 +0330

    Add base VM

---
+++ Weather/Weather/ViewModels/BaseVM.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.ViewModels
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChange(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private bool buzy { get; set; }
+
+        public async Task run(Func<Task> func)
+        {
+            if (!buzy)
+            {
+                buzy = true;
+                try{
+                    await func();
+                }
+                finally{
+                buzy = false;
+                }
+              
+            }
+        }
+    }
+}
